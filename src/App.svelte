5866db4f65a92451018231a1300065647bf7caca
glaisafaye <glaisafayeadora@gmail.com>
2024-03-05 13:32:42 +0800

    updated

--- src/App.svelte
+++ src/App.svelte
@@ -2,15 +2,25 @@
 	let age = 0;
 
 	const ageSubmit = (e) => {
-		age = (e.target.value);
-	}
-	
+		age = e.target.value;
+	};
 </script>
 
-	<input type="number" id="age" bind:value={age} on:input{ageSubmit}>
-		{#if age >= 18}
-			<p>You're adult</p>
-		{:else}
-			<p>You're young</p>
-		{/if}
+<input class="ageInput" type="number" id="age" bind:value={age} on:input{ageSubmit} />
+{#if age >= 18}
+	<p class="message">You're adult</p>
+{:else}
+	<p class="message">You're young</p>
+{/if}
 
+<style>
+	.ageInput {
+		width: 100px;
+		height: 30px;
+		padding: 10px;
+		text-align: center;
+	}
+	.message{
+		color: red;
+	}
+</style>
