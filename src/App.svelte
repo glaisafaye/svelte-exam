186e90cdd873f57721ece81301b5df72b36b3e8d
glaisafaye <glaisafayeadora@gmail.com>
2024-03-05 13:25:03 +0800

    svelte exam

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	let age = 0;
+
+	const ageSubmit = (e) => {
+		age = (e.target.value);
+	}
+	
+</script>
+
+	<input type="number" id="age" bind:value={age} on:input{ageSubmit}>
+		{#if age >= 18}
+			<p>You're adult</p>
+		{:else}
+			<p>You're young</p>
+		{/if}
+
