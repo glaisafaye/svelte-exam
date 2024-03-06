2eb871e008cf4f877ad06d4a35dab737adb91d7c
glaisafaye <glaisafayeadora@gmail.com>
2024-03-06 13:23:15 +0800

    updated

--- src/App.svelte
+++ src/App.svelte
@@ -8,18 +8,26 @@
 	};
 </script>
 
-<label for="text">Type your age:</label>
-<input class="ageInput" type="number" id="age" bind:value={age} on:input={ageSubmit} />
+<div class="container">
+	<label class="typeAge" for="text">Type your age:</label>
+	<input class="ageInput" type="number" id="age" bind:value={age} on:input={ageSubmit} />
 
-{#if ageSubmitted}
-	{#if age >= 18}
-		<p class="message">You're an adult</p>
-	{:else}
-		<p class="message">You're young</p>
+	{#if ageSubmitted}
+		{#if age >= 18}
+			<p class="message">You're an adult</p>
+		{:else}
+			<p class="message">You're young</p>
+		{/if}
 	{/if}
-{/if}
+</div>
 
 <style>
+	.container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
 	.ageInput {
 		width: 100px;
 		height: 30px;
@@ -27,7 +35,12 @@
 		text-align: center;
 		color: blue;
 	}
+
 	.message {
 		color: red;
+	}
+
+	.typeAge {
+		margin-bottom: 5px;
 	}
 </style>
